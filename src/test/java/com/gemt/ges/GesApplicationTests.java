86beae78b70d2880d0dd834794cca71f1f96a877
Giuliano Espejo Mezzabotta <129322090+Giuliano-Espejo@users.noreply.github.com>
2024-09-22 04:13:46 -0300

    first commit

---
+++ src/test/java/com/gemt/ges/GesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gemt.ges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
